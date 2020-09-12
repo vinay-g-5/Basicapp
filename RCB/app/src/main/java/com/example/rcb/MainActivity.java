130c16e1a035344a578b92ae453f49344e3bbe3d
vinay <vinay82545@gmail>
2020-09-12 21:07:34 +0530

    intail

---
+++ RCB/app/src/main/java/com/example/rcb/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.rcb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvmsg;
+    private ImageView ivmg;
+    private EditText edmsg;
+    private Button btncon;
+    private ImageView ivrcb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvmsg=findViewById(R.id.tvMsg);
+        ivmg=findViewById(R.id.ivmg);
+        edmsg=findViewById(R.id.edMsg);
+        btncon=findViewById(R.id.btnConfirm);
+        ivrcb=findViewById(R.id.ivrcb);
+        btncon.setOnClickListener(v -> {
+            String msg=edmsg.getText().toString();
+
+            if(msg.isEmpty()){
+                Toast.makeText(this, "Message cn't be displayed!!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                tvmsg.setText(msg);
+            }
+        }
+    );
+    }
+}
